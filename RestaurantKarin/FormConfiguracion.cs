e7f2258e979a4f9fad61ca1a89608fdfe1a16577
emilio1914 <emiliofernando812@gmail.com>
2026-04-19 19:07:45 -0600

    se creo el from configuracion

---
+++ RestaurantKarin/FormConfiguracion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RestaurantKarin
+{
+    public partial class FormConfiguracion : Form
+    {
+        public FormConfiguracion()
+        {
+            InitializeComponent();
+        }
+    }
+}
